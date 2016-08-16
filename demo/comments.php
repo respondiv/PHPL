97fd321936a301aec14522fbfeb81f404dd5515e
Sushil Prasai <Sushil Prasai>
2016-08-15 21:49:50 -0600

    Initial commit

---
+++ demo/comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+
+//  This is a PHP Single Line Comment
+
+/* Following command prints / displayes text "Hello Student"
+
+and notice this is a multi line comment */
+
+echo "Hello Student";
+
+
+
+?>
+	
+</body>
+</html>
