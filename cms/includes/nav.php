63a2604977b3a50ed7e119b7706642fe3693df26
Sushil <sushil@respondiv.com>
2016-08-19 22:34:27 -0600

    working with CMS

--- cms/includes/nav.php
+++ cms/includes/nav.php
@@ -14,7 +14,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
+<!--                     <li>
                         <a href="#">About</a>
                     </li>
                     <li>
@@ -22,7 +22,25 @@
                     </li>
                     <li>
                         <a href="#">Contact</a>
-                    </li>
+                    </li> -->
+
+                    <?php
+                        $query = "SELECT * FROM categories";
+                        $select_all_categories = mysqli_query($connection,$query);
+
+                        if (!$select_all_categories) {
+                            die("Query FAILED " . mysqli_error($connection));
+                        }
+
+                        while($row = mysqli_fetch_assoc($select_all_categories)){
+                            $cat_title = $row['cat_title']; 
+
+                            echo "<li><a href='#''>{$cat_title}</a></li>";
+                        }
+
+
+                    ?>
+
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
--- cms/index.php
+++ cms/index.php
@@ -1,5 +1,7 @@
+<?php include "includes/db.php"                /* Include Navigation */ ?> 
 <?php include "includes/header.php"             /* Include Header */ ?> 
 <?php include "includes/nav.php"                /* Include Navigation */ ?> 
+
 
     
 
@@ -12,25 +14,53 @@
             <div class="col-md-8">
 
                 <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
+                    My First CMS
+                    <small>Fully Custom Made</small>
                 </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <?php
 
-                <hr>
+                    $query = "SELECT * FROM posts";
+                    $select_all_posts = mysqli_query($connection,$query);
+
+                    if (!$select_all_posts) {
+                        die("Query FAILED " . mysqli_error($connection));
+                    }
+
+                    while($row = mysqli_fetch_assoc($select_all_posts)){
+                        $post_id = $row['post_id']; 
+                        $post_category_id = $row['post_category_id']; 
+                        $post_title = $row['post_title']; 
+                        $post_author = $row['post_author']; 
+                        $post_date = $row['post_date']; 
+                        $post_image = $row['post_image']; 
+                        $post_content = $row['post_content']; 
+                        $post_tags = $row['post_tags'];
+                        $post_status = $row['post_status'];
+                        $post_comment_count = $row['post_comment_count'];
+                ?>    
+
+
+                    <!-- Blog Post -->
+                    <h2>
+                        <a href="#"><?php echo $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+                <?php } ?>
+
+
+                
 
                 
    
